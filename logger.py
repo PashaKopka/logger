b8985c14c92566b51684fa19c18f207a5499a46a
PashaKopka <pashakopka216@gmail.com>
2020-09-16 11:25:15 +0300

    string formating

--- logger.py
+++ logger.py
@@ -9,13 +9,22 @@ class Logger:
         self.log_file = log_file
         self.time = time.localtime()
 
-    def save_error(self, error):
-        current_time = self._get_time()
-        date = self._get_date()
-        error_doc = error.__doc__
+    def _make_log_string(self, error):
         file_name = traceback.StackSummary.extract(traceback.walk_stack(None))[1].filename
+        values = [self._get_time(), self._get_date(), file_name, error.__doc__]
+        string = ''
+        i = 0
+        for arg in values:
+            if (i % 3) == 0 and i:
+                string += '\n' + ' ' * (len(values[0]) + 3) if (len(values[0]) <= 10) else 10
+            string += f'[{arg}] '
+            i += 1
+
+        return string + '\n'
+
+    def save_error(self, error):
         with open(self.log_file, 'a') as log_file:
-            log_file.write(f'[{current_time}][{date}], File: "{file_name}", Error: {error_doc} \n')
+            log_file.write(self._make_log_string(error))
 
     def _get_time(self):
         return time.strftime("%H:%M:%S", self.time)
