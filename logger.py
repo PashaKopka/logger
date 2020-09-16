13cc788878df0ab4135d2bf1878727e57f98f3f7
PashaKopka <pashakopka216@gmail.com>
2020-09-16 11:08:10 +0300

    changes with datetime

--- logger.py
+++ logger.py
@@ -1,16 +1,24 @@
 import traceback
 from datetime import datetime
+import time
 
 
 class Logger:
 
     def __init__(self, log_file='log_file.txt'):
         self.log_file = log_file
+        self.time = time.localtime()
 
     def save_error(self, error):
-        time = datetime.now()
+        current_time = self._get_time()
+        date = self._get_date()
         error_doc = error.__doc__
         file_name = traceback.StackSummary.extract(traceback.walk_stack(None))[1].filename
         with open(self.log_file, 'a') as log_file:
-            log_file.write(f'[{time}], File: "{file_name}", Error: {error_doc}')
+            log_file.write(f'[{current_time}][{date}], File: "{file_name}", Error: {error_doc} \n')
 
+    def _get_time(self):
+        return time.strftime("%H:%M:%S", self.time)
+
+    def _get_date(self):
+        return datetime.now().date()
